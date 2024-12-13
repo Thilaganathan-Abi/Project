701c0bb91e19bb657f3ebf9948b6d816a9b7cd0e
T-A_B_I. <abivarna072000@gmail.com>
2024-12-13 03:40:02 +0000

    edit

---
+++ php/dbconf.php
@@ -0,0 +1,22 @@
+<?php
+// Constant variable (variable name, value)
+define('SERVERNAME', '127.0.0.1');
+define('USERNAME', 'root');
+define('PASSWORD','mariadb');
+define('DBNAME', 'library');
+try{
+    //connect with database
+    $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+
+    if(!$connect){
+        die("connection failed".mysqli_connect_error());//die - stop process after that
+    } else {
+       // echo "Connection successfully <br>";
+    }
+}
+catch(Exception $e){
+    die($e->getMessage());
+}
+
+//echo "abc <br>"
+?>
