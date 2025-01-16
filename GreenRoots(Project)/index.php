670f4f9532f8586a14d59df232f5c3a34173289c
T-A_B_I. <abivarna072000@gmail.com>
2025-01-16 17:58:33 +0000

    add colour

--- GreenRoots(Project)/index.php
+++ GreenRoots(Project)/index.php
@@ -4,8 +4,9 @@ require_once 'conf.php';
 //require_once 'loginpage.php'
 //require_once 'GreenRoots.php';
 if (isset($_SESSION['loginuser'])) {
-	echo "<h3>Hi user ".$_SESSION['loginuser']."</h3>";
-	echo "<a style='float:right' href='logout.php'>Logout</a>";
+	//echo "<h3>Hi user ".$_SESSION['loginuser']."</h3>";
+	//echo "<a style='float:right' href='logout.php'>Logout</a>";
+	echo "<a style='float:right; color: #fcff45; font-size: 16px;' href='logout.php'>Logout</a>";
 ?>
 <!DOCTYPE html>
 <html>
